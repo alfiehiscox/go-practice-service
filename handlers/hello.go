3603525553437467a748f93f5cd550416a9b02a5
Alfie Hiscox <alfie.hiscox@gmail.com>
2022-09-13 18:12:29 +0100

    go format on that guy

--- handlers/hello.go
+++ handlers/hello.go
@@ -1,23 +1,22 @@
 package handlers
 
 import (
-    "net/http"
-    "log"
-    "fmt"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 type Hello struct {
-    l *log.Logger
+	l *log.Logger
 }
 
 // Constructor
 func NewHello(l *log.Logger) *Hello {
-    return &Hello{l}
+	return &Hello{l}
 }
 
 // HTTP Handler
 func (h *Hello) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-    h.l.Println("Received Request to Hello Handler")
-    fmt.Fprint(rw, "Hello, world!")
+	h.l.Println("Received Request to Hello Handler")
+	fmt.Fprint(rw, "Hello, world!")
 }
-
--- main.go
+++ main.go
@@ -1,55 +1,55 @@
 package main
 
 import (
-    "net/http"
-    "os"
-    "os/signal"
-    "context"
-    "log"
-    "time"
-    "github.com/alfiehiscox/go-practice-service/handlers"
+	"context"
+	"github.com/alfiehiscox/go-practice-service/handlers"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-    l := log.New(os.Stdout, " go-practice-service", log.LstdFlags)
-    
-    // Handler
-    hh := handlers.NewHello(l)
+	l := log.New(os.Stdout, " go-practice-service", log.LstdFlags)
 
-    // ServeMux on index of port
-    sm := http.NewServeMux()
-    sm.Handle("/", hh)
+	// Handler
+	hh := handlers.NewHello(l)
 
-    // Server Configuration
-    s := http.Server{
-        Addr: ":8080",
-        Handler: sm,
-        ErrorLog: l,
-        ReadTimeout: 5 * time.Second, 
-        WriteTimeout: 10 * time.Second,
-        IdleTimeout: 120 * time.Second,
-    }
+	// ServeMux on index of port
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
 
-    // Using goroutine to handle exiting gracefully
-    go func() {
-        l.Println("Started listening on Port: 8080")
-        err := s.ListenAndServe()
-        if err != nil {
-            l.Printf("Server Info: %s\n", err)
-            os.Exit(1)
-        }
-    }()
+	// Server Configuration
+	s := http.Server{
+		Addr:         ":8080",
+		Handler:      sm,
+		ErrorLog:     l,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
 
-    // Make Channel to listen to signals on the goroutine
-    c := make(chan os.Signal, 1)
-    signal.Notify(c, os.Interrupt)
-    signal.Notify(c, os.Kill)
+	// Using goroutine to handle exiting gracefully
+	go func() {
+		l.Println("Started listening on Port: 8080")
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Printf("Server Info: %s\n", err)
+			os.Exit(1)
+		}
+	}()
 
-    // Listen to the channel and exit gracefully
-    sig := <-c
-    l.Println("Got Signal: ", sig)
-    ctx, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
-    defer cancel()
-    s.Shutdown(ctx)
+	// Make Channel to listen to signals on the goroutine
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	// Listen to the channel and exit gracefully
+	sig := <-c
+	l.Println("Got Signal: ", sig)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	s.Shutdown(ctx)
 
 }
